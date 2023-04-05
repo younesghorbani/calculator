19d53c5e8e280b1dfd54c9c1135f468fada14718
Younes Ghorbani <122295811+younesghorbani@users.noreply.github.com>
2023-04-05 17:44:09 +0330

    Add basic math operation functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
